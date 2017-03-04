4a1bf73c86eda0b8c917ea8b17f806a914348dd6
Jeannine Hall <jeanninehall@dhcp-10-105-163-112.wireless.northwestern.private>
2017-03-04 09:43:32 -0600

    final commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	// start the form
+   // include a countdown timer.
+   var timer;
+
+   function countdownTimer () {
+   		timer = setTimeout(gameOver, 120,000);
+   }
+
+   function gameOver() {
+   	alert ("Game Over");
+   }
+   // count correct answers
+   // count incorrect answers
+   // reveal the number of questions that players answer correctly and incorrectly
+
+});
